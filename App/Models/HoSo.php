11df8ca4aa418accb0e6d5e40d51183559ad8713
Huu Ty <typrone1@gmail.com>
2017-11-02 10:28:25 +0700

    Quen checkout

--- App/Models/HoSo.php
+++ App/Models/HoSo.php
@@ -76,9 +76,70 @@ class HoSo extends Model
     public function save()
     {
         $this->validate();
+        $file_max_weight = 1000000; //limit the maximum size of file allowed (20Mb)
+        $ok_ext = array('jpg','png','gif','jpeg'); // allow only these types of files
+        $destination = '../public/images/AnhHoSo/'; // where our files will be stored
+        $file = $_FILES['hinhAnh'];
+        $fileNewName ='';
+        if (
+            !isset($_FILES['hinhAnh']['error']) ||
+            is_array($_FILES['hinhAnh']['error'])
+        ) {
+            throw new \RuntimeException('Invalid parameters.');
+        }
+        else {
+            $filename = explode(".", $file["name"]);
+            $file_name = $file['name']; // file original name
+
+            $file_name_no_ext = isset($filename[0]) ? $filename[0] : null; // File name without the extension
+
+            $file_extension = $filename[count($filename)-1];
+
+            $file_weight = $file['size'];
+
+            $file_type = $file['type'];
+            if( $file['error'] == 0 )
+            {
+                // check if the extension is accepted
+                if( in_array($file_extension, $ok_ext)):
+
+                    // check if the size is not beyond expected size
+                    if( $file_weight <= $file_max_weight ):
+
+
+                        // rename the file
+                        $fileNewName = md5( $file_name_no_ext[0].microtime() ).'.'.$file_extension ;
+
+
+                        // and move it to the destination folder
+                        if( move_uploaded_file($file['tmp_name'], $destination.$fileNewName) ):
+
+                            echo" File uploaded !";
+
+                        else:
+
+                            echo "can't upload file.";
+
+                        endif;
+
+
+                    else:
+
+                        echo "File too heavy.";
+
+                    endif;
+
+
+                else:
+
+                    echo "File type is not supported.";
+
+                endif;
+            }
+        }
         if (empty($this->errors)) {
-            $sql = 'INSERT INTO HoSo (MaHoToc, HoTen, NgaySinh, GioiTinh, DoiThu, ConThu, MaHoSoBo)
-            VALUES (1, :hoTen, :ngaySinh, :gioiTinh, :doiThu, :conThu, :maHoSoBo)';
+            $sql = 'INSERT INTO HoSo (MaHoToc, HoTen, NgaySinh, GioiTinh, DoiThu, ConThu, MaHoSoBo, HinhAnh)
+            VALUES (1, :hoTen, :ngaySinh, :gioiTinh, :doiThu, :conThu, :maHoSoBo, :hinhAnh)';
             $db = static::getDB();
             $stmt = $db->prepare($sql);
             $stmt->bindValue(':hoTen', $this->hoTen, PDO::PARAM_STR);
@@ -87,6 +148,7 @@ class HoSo extends Model
             $stmt->bindValue(':doiThu', $this->doiThu, PDO::PARAM_INT);
             $stmt->bindValue(':conThu', $this->conThu, PDO::PARAM_INT);
             $stmt->bindValue(':maHoSoBo', $this->maHoSoBo?$this->maHoSoBo: null, PDO::PARAM_INT);
+            $stmt->bindValue(':hinhAnh', $fileNewName, PDO::PARAM_STR);
             return $stmt->execute();
         }
     }
--- public/js/app.js
+++ public/js/app.js
@@ -15,19 +15,19 @@ $('.box-item button').click(function(){
 $(".tree li:not(li:has(ul))").each(function () {
     $(this).find('button').remove();
 });
-$.validator.addMethod('validPassword',
-    function(value, element, param) {
-
-        if (value != '') {
-            if (value.match(/.*[a-z]+.*/i) == null) {
-                return false;
-            }
-            if (value.match(/.*\d+.*/) == null) {
-                return false;
-            }
-        }
-
-        return true;
-    },
-    'Must contain at least one letter and one number'
-);
+// $.validator.addMethod('validPassword',
+//     function(value, element, param) {
+//
+//         if (value != '') {
+//             if (value.match(/.*[a-z]+.*/i) == null) {
+//                 return false;
+//             }
+//             if (value.match(/.*\d+.*/) == null) {
+//                 return false;
+//             }
+//         }
+//
+//         return true;
+//     },
+//     'Must contain at least one letter and one number'
+// );
