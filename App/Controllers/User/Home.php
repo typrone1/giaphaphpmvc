6f6ecc246908f9c8032df8046e5a978d6df0bf67
Huu Ty <typrone1@gmail.com>
2017-11-08 11:10:15 +0700

    Them lich vao giao dien

--- App/Controllers/User/Home.php
+++ App/Controllers/User/Home.php
@@ -6,7 +6,7 @@ use App\Controllers\Authenticated;
 use App\Models\BaiViet;
 use Core\Controller;
 use Core\View;
-class Home extends Authenticated
+class Home extends Controller
 {
     public function indexAction(){
         View::renderTemplate('User/index.html', ['dsBaiViet' => BaiViet::getAll()]);
